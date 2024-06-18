4a49a1218285940722438fa55c67a6843eb2de8b
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-06-18 17:58:57 +0200

    Nightingale song integrated

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     showScreen('startScreen');
     document.getElementById('startButton').addEventListener('click', startGame);
 });
+
+function toggleNightingaleSong() {
+    const audioElement = document.getElementById('nightingaleAudio');
+
+    if (audioElement.paused) {
+        audioElement.play();
+    } else {
+        audioElement.pause();
+        audioElement.currentTime = 0; // Reset audio to start
+    }
+}
